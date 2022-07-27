d047c2daacb236955874db564db8f9769f5ba008
Sushil Kumar <sushilz25558@gmail.com>
2022-07-27 13:32:01 +0530

    server branch inti

--- apps/client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './app'
-import {DbContext} from './db'
-ReactDOM.render(<DbContext><App /></DbContext>, document.querySelector('#root'));
